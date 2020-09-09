5e964e7a0b6ee3172b0c87882190c129515e312f
Dmitriy Rassolov <dmitriy.rassolov@gmail.com>
2020-09-09 08:54:38 +0300

    restructure index

--- src/index.js
+++ src/index.js
@@ -12,53 +12,60 @@ import {TicTacToe} from "./examples/tictactoe";
 import {ProtoShop} from "./examples/protoshop";
 import {Clock} from "./examples/clock";
 import {ReactDeactivator} from "./examples/reactdeactivator";
+import ContextExamples from "./examples/context/contextexamples";
 
 function App() {
     return (
         <>
-            <div>
-                <p>Tutorial: Intro to React</p>
-                <TicTacToe/>
-                <hr />
-            </div>
-            <div>
-                <p>State and Lifecycle</p>
-                <Clock/>
-                <ReactDeactivator rootId={"root"}/>
-                <hr />
-            </div>
-            <div>
-                <p>Handling Events</p>
-                <Toggle/>
-                <Page/>
-                <hr />
-            </div>
-            <div>
-                <p>Conditional Rendering</p>
-                <LoginControl/>
-                <hr />
-            </div>
-            <div>
-                <p>List and Keys</p>
-                <NumberList numbers={[1, 2, 3, 4]}/>
-                <hr />
-            </div>
-            <div>
-                <p>Forms (uncontrolled)</p>
-                <Nameform/>
-                <SimpleTextEditor/>
-                <hr />
-            </div>
-            <div>
-                <p>Lifting State up</p>
-                <TemperatureCalculator/>
-                <hr />
-            </div>
-            <div>
-                <p>Thinking in React</p>
-                <ProtoShop/>
-                <hr />
-            </div>
+            <h1>Tutorial</h1>
+            <details>
+                <div>
+                    <p>Intro to React</p>
+                    <TicTacToe/>
+                    <hr/>
+                </div>
+            </details>
+            <h1>Main concepts</h1>
+            <details>
+                <div>
+                    <p>State and Lifecycle</p>
+                    <Clock/>
+                    <ReactDeactivator rootId={"root"}/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>Handling Events</p>
+                    <Toggle/>
+                    <Page/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>Conditional Rendering</p>
+                    <LoginControl/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>List and Keys</p>
+                    <NumberList numbers={[1, 2, 3, 4]}/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>Forms (uncontrolled)</p>
+                    <Nameform/>
+                    <SimpleTextEditor/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>Lifting State up</p>
+                    <TemperatureCalculator/>
+                    <hr/>
+                </div>
+                <div>
+                    <p>Thinking in React</p>
+                    <ProtoShop/>
+                    <hr/>
+                </div>
+            </details>
         </>
     );
 }
