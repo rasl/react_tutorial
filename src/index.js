2643f504884f6a6bd20e2767f1c548fa7d0644dd
Dmitriy Rassolov <dmitriy.rassolov@gmail.com>
2020-09-12 03:11:48 +0300

    add error boundary

--- src/index.js
+++ src/index.js
@@ -4,17 +4,17 @@ import './index.css';
 import {default  as TutorialBlock} from "./examples/tutorial/IndexBlock";
 import {default as MainConceptsBlock} from "./examples/mainConcepts/IndexBlock";
 import {default as AdvancedGuidesBlock} from "./examples/advancedGuides/IndexBlock";
+import ErrorBoundary from "./examples/advancedGuides/ErrorBoundary";
 
 function App() {
     return (
-        <>
+        <ErrorBoundary>
             <TutorialBlock/>
             <MainConceptsBlock/>
             <AdvancedGuidesBlock/>
-        </>
+        </ErrorBoundary>
     );
 }
-
 
 ReactDOM.render(
     <App/>,
